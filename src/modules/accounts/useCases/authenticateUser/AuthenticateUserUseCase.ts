bcacf0f316f085bfe4b8df1a499c35dae41fc7b2
devmateusramos <devmateusramos@gmail.com>
2022-05-30 20:42:05 -0300

    carros e login

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,60 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { AppError } from '../../../../errors/AppError';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Email or password incorrect! user');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    console.log(passwordMatch);
+
+    if (!passwordMatch) {
+      throw new AppError('Email or password incorrect! password');
+    }
+
+    const token = sign({}, 'asiojdoia213sjasd312doja', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
---
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -0,0 +1,40 @@
+import { getRepository, Repository } from "typeorm";
+
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+export class CategoriesRepository implements ICategoriesRepository {
+  private repository: Repository<Category>;
+
+  private static INSTANCE: CategoriesRepository;
+
+  constructor() {
+    this.repository = getRepository(Category);
+  }
+
+  async create({ description, name }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({
+      description,
+      name,
+    });
+
+    await this.repository.save(category);
+  }
+
+  async list(): Promise<Category[]> {
+    const categories = await this.repository.find();
+
+    return categories;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    // Select * from categories where name = "name"  o findOne poe um limit 1
+    // utilizar o "{}" seria por o where
+    const category = await this.repository.findOne({ name });
+
+    return category;
+  }
+}
